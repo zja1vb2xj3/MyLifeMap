080af688cb65cf9afe7b842ab579cfa5e3036056
PDG <zja1vb2xj3@naver.com>
2017-09-15 23:39:09 +0900

    상태바, 액션바 , 그라데이션 적용

--- app/src/main/java/com/example/user/mylifemap/MainActivity.java
+++ app/src/main/java/com/example/user/mylifemap/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.mylifemap;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorBlack));//윈도우 색상 변경
+            getSupportActionBar().setBackgroundDrawable(getDrawable(R.color.colorBlack));//액션바 색상 변경
+        }
     }
 }
